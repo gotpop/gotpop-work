c0ad61999b769403097f1f9df84ab2442fd331af
GotPop <gotpop@users.noreply.github.com>
2025-12-30 14:29:15 +0000

    Adding cross work nav transitions

--- public/scripts/view-transitions.js
+++ public/scripts/view-transitions.js
@@ -1,12 +1,47 @@
+const workPosts = [
+  "blexr",
+  "audio-network",
+  "aetna",
+  "magnum-ice-cream",
+  "coats",
+  "virgin-radio",
+  "many-pets",
+  "hackett-menswear",
+  "discovery",
+  "camelot",
+]
+
 const getTransitionType = (fromUrl, toUrl) => {
   try {
     const fromPath = new URL(fromUrl).pathname
     const toPath = new URL(toUrl).pathname
 
-    const fromPost = fromPath.startsWith("/work/")
-    const toPost = toPath.startsWith("/work/")
+    const fromMatch = fromPath.match(/^\/work\/(.+)$/)
+    const toMatch = toPath.match(/^\/work\/(.+)$/)
 
-    if (fromPost && toPost) return "post-to-post"
+    if (fromMatch && toMatch) {
+      const fromSlug = fromMatch[1]
+      const toSlug = toMatch[1]
+
+      const fromIndex = workPosts.indexOf(fromSlug)
+      const toIndex = workPosts.indexOf(toSlug)
+
+      if (fromIndex !== -1 && toIndex !== -1) {
+        const forwardDistance =
+          (toIndex - fromIndex + workPosts.length) % workPosts.length
+        const backwardDistance =
+          (fromIndex - toIndex + workPosts.length) % workPosts.length
+
+        if (forwardDistance < backwardDistance) {
+          return "post-to-post-forward"
+        }
+        if (backwardDistance < forwardDistance) {
+          return "post-to-post-backward"
+        }
+      }
+
+      return "post-to-post"
+    }
 
     return null
   } catch {
